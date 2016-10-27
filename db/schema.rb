fc6e1e950f860df0eb6e073072830d53b406a38e
brandonbaah <brandonbaah@gmail.com>
2016-10-27 19:38:21 -0400

    added show action and view for ratings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027165240) do
+ActiveRecord::Schema.define(version: 20161027230015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 20161027165240) do
     t.string   "email"
     t.string   "phone_number"
     t.string   "token"
-    t.boolean  "status"
+    t.boolean  "tax_exempt"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
