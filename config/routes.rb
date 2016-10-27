7da566c40e4bccb2c91855ee1328b1ea70c21165
Tom Yang <xy2286@columbia.edu>
2016-10-26 20:49:05 -0400

    admin panel initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
 
   get '/' => 'welcome#index'
 
+
   get 'ratings/new' => 'ratings#new'
   post 'ratings' => 'ratings#create'
+
+  get '/admins' => 'admins#index'
+
+
 
 end
