293012962ead1b39f37ea813fb32a3885abfc3c6
Claudia McCormack <mccormack.chc@gmail.com>
2016-11-17 20:49:14 -0500

    questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,15 @@
 class Question < ApplicationRecord
   has_many :answers
   has_many :users, through: :answers
+
+  # This is where we write the word count checker method
+  # validate for under word count
+  # Check the answer_text and compare it to the question word Limit
+  # Add error message for too-long answer
+
+  # form input text_area can also have a word count limit on it!
+
+  # Same thing with required questions: an is_required validation will check if
+  # the user has skipped a required question
+
 end
