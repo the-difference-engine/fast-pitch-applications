1aced06006830cc9c443a9ffb2bbe5c6fa840b51
Claudia McCormack <mccormack.chc@gmail.com>
2016-11-17 19:18:53 -0500

    updated questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,102 +1,118 @@
-Question.create(
-  question_text: "Briefly describe what your organization does. What product(s) and/or services(s) does your nonprofit offer?"
-)
+def seed_database
+  seed_questions
+  seed_user
+  seed_admin
+  seed_answer
+end
 
-Question.create(
-  question_text: "List the social issues your nonprofit addresses."
-)
+def seed_questions
+  Question.destroy_all
+  Question.create(question_text: "Organization")
+  Question.create(question_text: "Application Contact")
+  Question.create(question_text: "Title")
+  Question.create(question_text: "Phone")
+  Question.create(question_text: "Email")
+  Question.create(question_text: "Address")
+  Question.create(question_text: "Proposed Participant")
+  Question.create(question_text: "Title")
+  Question.create(question_text: "Phone")
+  Question.create(question_text: "Email")
+  Question.create(question_text: "Date Organized")
+  Question.create(question_text: "Number of Full-Time Employees")
+  Question.create(question_text: "Number of Part-Time Employees")
+  Question.create(question_text: "Budget for current fiscal year")
+  Question.create(question_text: "What are your current sources of funding (include percentages)?")
+  Question.create(question_text: "Do you have your 501c3?")
+  Question.create(question_text: "If not, do you have a fiscal agent?")
+  Question.create(question_text: "Name of fiscal agent (if applicable)")
+  Question.create(question_text: "Sector(s) your programs address")
+  Question.create(question_text: "Briefly describe what your organization does (Limit 300 words)")
+  Question.create(question_text: "What communities do you serve?")
+  Question.create(question_text: "Who do you help (target population)?")
+  Question.create(question_text: "How does your nonprofit make a meaningful impact (Limit 200 words)")
+  Question.create(question_text: "How do you measure the impact of your nonprofit? Please share any relevant statistics.")
+  Question.create(question_text: "What differentiates you from other organizations working to address the same issues? (Limit 300 words)")
+  Question.create(question_text: "What are your organization's goals for the coming year (Limit 300 words)")
+  Question.create(question_text: "What do you hope to gain from participating in Fast Pitch? (Limit 100 words)")
+  Question.create(question_text: "What is the one thing you most want us to know about your organization (Limit 200 words)")
+end
 
-Question.create(
-  question_text: "Why do you believe that your nonprofit can make a meaningful impact in addressing the issues identified above?"
-)
+def seed_user
+  User.destroy_all
+  User.create(
+    name: "Hope NY",
+    contact_name: "Lebron James",
+    email: "hopeny@example.com",
+    phone_number: "(718)542-4213",
+    tax_exempt: true
+  )
+end
 
-Question.create(
-  question_text: "What differentiates you from other organizations working to address the same issues?"
-)
+def seed_admin
+  Admin.destroy_all
+  Admin.create(
+    name: "Harper Coven",
+    email: "admin4@example.com",
+    encrypted_password: "password",
+    super_admin: false
+  )
 
-Question.create(
-  question_text: "What demographic does your nonprofit serve?"
-)
+  Admin.create(
+    name: "Summer Jones",
+    email: "admin5@example.com",
+    encrypted_password: "password",
+    super_admin: true
+  )
 
-Question.create(
-  question_text: "How do you measure the impact of your nonprofit? Please share any relevant statistics."
-)
+  Admin.create(
+    name: "Henry Lee",
+    email: "admin2@example.com",
+    encrypted_password: "password",
+    super_admin: true
+  )
 
-Question.create(
-  question_text: "What are the top 3 strategic goals for your nonprofit in the coming year?"
-)
+  Admin.create(
+    name: "Eliza Day",
+    email: "admin6@example.com",
+    encrypted_password: "password",
+    super_admin: false
+  )
+end
 
-Question.create(
-  question_text: "What are your organization's sources of revenue?"
-)
+def seed_answer
+  Answer.destroy_all
+  Answer.create(user_id: 1, question_id: 1, answer_text: "1bark bark bark")
+  Answer.create(user_id: 1, question_id: 2, answer_text: "b2ark bark bark")
+  Answer.create(user_id: 1, question_id: 3, answer_text: "ba3rk bark bark")
+  Answer.create(user_id: 1, question_id: 4, answer_text: "bar4k bark bark")
+  Answer.create(user_id: 1, question_id: 5, answer_text: "bark5 bark bark")
+  Answer.create(user_id: 1, question_id: 6, answer_text: "bark 6bark bark")
+  Answer.create(user_id: 1, question_id: 7, answer_text: "bark b7ark bark")
+  Answer.create(user_id: 1, question_id: 8, answer_text: "bark bar8k bark")
+  Answer.create(user_id: 1, question_id: 9, answer_text: "bark bark9 bark")
+  Answer.create(user_id: 1, question_id: 10, answer_text: "bark bark10 bark")
 
-Question.create(
-  question_text: "How many full time (FTE equivalent) employees do you employ?"
-)
+  Answer.create(user_id: 2, question_id: 1, answer_text: "1meow meow meow")
+  Answer.create(user_id: 2, question_id: 2, answer_text: "m2eow meow meow")
+  Answer.create(user_id: 2, question_id: 3, answer_text: "me3ow meow meow")
+  Answer.create(user_id: 2, question_id: 4, answer_text: "meo4w meow meow")
+  Answer.create(user_id: 2, question_id: 5, answer_text: "meow5 meow meow")
+  Answer.create(user_id: 2, question_id: 6, answer_text: "meow 6meow meow")
+  Answer.create(user_id: 2, question_id: 7, answer_text: "meow m7eow meow")
+  Answer.create(user_id: 2, question_id: 8, answer_text: "meow me8ow meow")
+  Answer.create(user_id: 2, question_id: 9, answer_text: "meow meo9w meow")
+  Answer.create(user_id: 2, question_id: 10, answer_text: "meow meo10w meow")
 
-Question.create(
-  question_text: "What is your current Elevator Pitch?"
-)
+  Answer.create(user_id: 3, question_id: 1, answer_text: "1chirp chirp chirp")
+  Answer.create(user_id: 3, question_id: 2, answer_text: "2chirp chirp chirp")
+  Answer.create(user_id: 3, question_id: 3, answer_text: "3chirp chirp chirp")
+  Answer.create(user_id: 3, question_id: 4, answer_text: "4chirp chirp chirp")
+  Answer.create(user_id: 3, question_id: 5, answer_text: "c5hirp chirp chirp")
+  Answer.create(user_id: 3, question_id: 6, answer_text: "ch6irp chirp chirp")
+  Answer.create(user_id: 3, question_id: 7, answer_text: "chi7rp chirp chirp")
+  Answer.create(user_id: 3, question_id: 8, answer_text: "chir8p chirp chirp")
+  Answer.create(user_id: 3, question_id: 9, answer_text: "chirp9 chirp chirp")
+  Answer.create(user_id: 3, question_id: 10, answer_text: "chirp10 chirp chirp")
+end
 
-Question.create(
-  question_text: "How would your organization benefit from participating in the fast pitch?"
-)
-
-Question.create(
-  question_text: "What types of services (broadly speaking) does your organization provide?"
-)
-
-User.create(
-  name: "Hope NY",
-  contact_name: "Lebron James",
-  email: "hopeny@example.com",
-  phone_number: "(718)542-4213",
-  tax_exempt: true
-)
-
-Admin.create(
-  name: "Harper Coven",
-  email: "admin4@example.com",
-  encrypted_password: "password",
-  super_admin: false
-)
-
-Admin.create(
-  name: "Summer Jones",
-  email: "admin5@example.com",
-  encrypted_password: "password",
-  super_admin: true
-)
-
-Answer.create(user_id: 1, question_id: 1, answer_text: "1bark bark bark")
-Answer.create(user_id: 1, question_id: 2, answer_text: "b2ark bark bark")
-Answer.create(user_id: 1, question_id: 3, answer_text: "ba3rk bark bark")
-Answer.create(user_id: 1, question_id: 4, answer_text: "bar4k bark bark")
-Answer.create(user_id: 1, question_id: 5, answer_text: "bark5 bark bark")
-Answer.create(user_id: 1, question_id: 6, answer_text: "bark 6bark bark")
-Answer.create(user_id: 1, question_id: 7, answer_text: "bark b7ark bark")
-Answer.create(user_id: 1, question_id: 8, answer_text: "bark bar8k bark")
-Answer.create(user_id: 1, question_id: 9, answer_text: "bark bark9 bark")
-Answer.create(user_id: 1, question_id: 10, answer_text: "bark bark10 bark")
-
-Answer.create(user_id: 2, question_id: 1, answer_text: "1meow meow meow")
-Answer.create(user_id: 2, question_id: 2, answer_text: "m2eow meow meow")
-Answer.create(user_id: 2, question_id: 3, answer_text: "me3ow meow meow")
-Answer.create(user_id: 2, question_id: 4, answer_text: "meo4w meow meow")
-Answer.create(user_id: 2, question_id: 5, answer_text: "meow5 meow meow")
-Answer.create(user_id: 2, question_id: 6, answer_text: "meow 6meow meow")
-Answer.create(user_id: 2, question_id: 7, answer_text: "meow m7eow meow")
-Answer.create(user_id: 2, question_id: 8, answer_text: "meow me8ow meow")
-Answer.create(user_id: 2, question_id: 9, answer_text: "meow meo9w meow")
-Answer.create(user_id: 2, question_id: 10, answer_text: "meow meo10w meow")
-
-Answer.create(user_id: 3, question_id: 1, answer_text: "1chirp chirp chirp")
-Answer.create(user_id: 3, question_id: 2, answer_text: "2chirp chirp chirp")
-Answer.create(user_id: 3, question_id: 3, answer_text: "3chirp chirp chirp")
-Answer.create(user_id: 3, question_id: 4, answer_text: "4chirp chirp chirp")
-Answer.create(user_id: 3, question_id: 5, answer_text: "c5hirp chirp chirp")
-Answer.create(user_id: 3, question_id: 6, answer_text: "ch6irp chirp chirp")
-Answer.create(user_id: 3, question_id: 7, answer_text: "chi7rp chirp chirp")
-Answer.create(user_id: 3, question_id: 8, answer_text: "chir8p chirp chirp")
-Answer.create(user_id: 3, question_id: 9, answer_text: "chirp9 chirp chirp")
-Answer.create(user_id: 3, question_id: 10, answer_text: "chirp10 chirp chirp")
+seed_database
