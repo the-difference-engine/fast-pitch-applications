a4f1e215cebf23b6fd760345d4414a8af1d031e2
Claudia McCormack <mccormack.chc@gmail.com>
2016-11-17 19:58:54 -0500

    updated dummy answers

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'rails_12factor', group: :production
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'faker'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def seed_database
   seed_questions
   seed_user
@@ -46,6 +47,22 @@ def seed_user
     phone_number: "(718)542-4213",
     tax_exempt: true
   )
+
+  User.create(
+    name: "Greenpoint",
+    contact_name: "Claudia McCormack",
+    email: "claudia@example.com",
+    phone_number: "(347) 123-4567",
+    tax_exempt: true
+  )
+
+  User.create(
+    name: "Brooklyn Charity",
+    contact_name: "Mary South",
+    email: "mary@example.com",
+    phone_number: "(347) 123-4567",
+    tax_exempt: true
+  )
 end
 
 def seed_admin
@@ -81,38 +98,24 @@ end
 
 def seed_answer
   Answer.destroy_all
-  Answer.create(user_id: 1, question_id: 1, answer_text: "1bark bark bark")
-  Answer.create(user_id: 1, question_id: 2, answer_text: "b2ark bark bark")
-  Answer.create(user_id: 1, question_id: 3, answer_text: "ba3rk bark bark")
-  Answer.create(user_id: 1, question_id: 4, answer_text: "bar4k bark bark")
-  Answer.create(user_id: 1, question_id: 5, answer_text: "bark5 bark bark")
-  Answer.create(user_id: 1, question_id: 6, answer_text: "bark 6bark bark")
-  Answer.create(user_id: 1, question_id: 7, answer_text: "bark b7ark bark")
-  Answer.create(user_id: 1, question_id: 8, answer_text: "bark bar8k bark")
-  Answer.create(user_id: 1, question_id: 9, answer_text: "bark bark9 bark")
-  Answer.create(user_id: 1, question_id: 10, answer_text: "bark bark10 bark")
+  questions = Question.all
 
-  Answer.create(user_id: 2, question_id: 1, answer_text: "1meow meow meow")
-  Answer.create(user_id: 2, question_id: 2, answer_text: "m2eow meow meow")
-  Answer.create(user_id: 2, question_id: 3, answer_text: "me3ow meow meow")
-  Answer.create(user_id: 2, question_id: 4, answer_text: "meo4w meow meow")
-  Answer.create(user_id: 2, question_id: 5, answer_text: "meow5 meow meow")
-  Answer.create(user_id: 2, question_id: 6, answer_text: "meow 6meow meow")
-  Answer.create(user_id: 2, question_id: 7, answer_text: "meow m7eow meow")
-  Answer.create(user_id: 2, question_id: 8, answer_text: "meow me8ow meow")
-  Answer.create(user_id: 2, question_id: 9, answer_text: "meow meo9w meow")
-  Answer.create(user_id: 2, question_id: 10, answer_text: "meow meo10w meow")
+  user = User.first
+  questions.each do |q|
+  # binding.pry
+    Answer.create(user_id: user.id, question_id: q.id, answer_text: Faker::StarWars.quote)
+  end
 
-  Answer.create(user_id: 3, question_id: 1, answer_text: "1chirp chirp chirp")
-  Answer.create(user_id: 3, question_id: 2, answer_text: "2chirp chirp chirp")
-  Answer.create(user_id: 3, question_id: 3, answer_text: "3chirp chirp chirp")
-  Answer.create(user_id: 3, question_id: 4, answer_text: "4chirp chirp chirp")
-  Answer.create(user_id: 3, question_id: 5, answer_text: "c5hirp chirp chirp")
-  Answer.create(user_id: 3, question_id: 6, answer_text: "ch6irp chirp chirp")
-  Answer.create(user_id: 3, question_id: 7, answer_text: "chi7rp chirp chirp")
-  Answer.create(user_id: 3, question_id: 8, answer_text: "chir8p chirp chirp")
-  Answer.create(user_id: 3, question_id: 9, answer_text: "chirp9 chirp chirp")
-  Answer.create(user_id: 3, question_id: 10, answer_text: "chirp10 chirp chirp")
+  user = User.second
+  questions.each do |q|
+    Answer.create(user_id: user.id, question_id: q.id, answer_text: Faker::StarWars.quote)
+  end
+
+  user = User.third
+  questions.each do |q|
+    Answer.create(user_id: user.id, question_id: q.id, answer_text: Faker::StarWars.quote)
+  end
 end
+
 
 seed_database
