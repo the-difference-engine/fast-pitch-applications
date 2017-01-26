8d71c9befefcdad10f602daca3ce8ad6209844e6
Claudia McCormack <mccormack.chc@gmail.com>
2017-01-26 18:25:18 -0500

    mailer debugged

---
+++ app/mailers/applicant_mailer.rb
@@ -0,0 +1,2 @@
+class ApplicantMailer < ApplicationMailer
+end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'no-reply@svp.com'
   layout 'mailer'
+
+  def confirmation(applicant)
+    @applicant = applicant
+    @url = "/"
+    mail(to: @applicant.email, subject: "Thank You for Starting Your Fast Pitch Application!")
+  end
+
 end
